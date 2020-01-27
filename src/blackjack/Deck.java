f0574c35398c05af311f70df0313dfb3c2d1c849
The Albino Giannis #14 <heidt007@gannon.edu>
2020-01-27 17:28:22 -0500

    Complete deck class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,35 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
+    private Card[] myCards = new Card[52]; //use new keyword because Card is an object
     
+    private String[] ranks = {}; //literally defining the array, so we use curly braces
+    private String[] suits = {}; //encapsulated because they are private, ensure no-one touches them
+    
+    private int nextCard = 0;
+    
+    public Deck(){ //empty constructor, it is its own return type
+        
+    }
+    
+    private void initDeck(){
+        
+    }
+    
+    private void shuffle(){
+        
+    }
+    
+    //printDeck is for testing only --delete me
+    public void printDeck(){
+        
+    }
+    
+    public Card dealCard(){ //a class is the data type of an object
+        
+        return null; //do not forget to change this 
+    }
+        
+        
+        
 }
