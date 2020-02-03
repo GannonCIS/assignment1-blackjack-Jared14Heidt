27275d442510973c5f656fdfb21463d208786f80
The Albino Giannis #14 <heidt007@gannon.edu>
2020-02-03 15:58:47 -0500

    implement init deck and add printdeck for testing

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,17 +12,23 @@ package blackjack;
 public class Deck {
     private Card[] myCards = new Card[52]; //use new keyword because Card is an object
     
-    private String[] ranks = {}; //literally defining the array, so we use curly braces
-    private String[] suits = {}; //encapsulated because they are private, ensure no-one touches them
+    private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"}; //literally defining the array, so we use curly braces
+    private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"}; //encapsulated because they are private, ensure no-one touches them
     
     private int nextCard = 0;
     
     public Deck(){ //empty constructor, it is its own return type
-        
+        initDeck(); //whenever I create a deck it will initialize the deck
     }
     
     private void initDeck(){
-        
+        int index = 0;
+        for(int s = 0; s < suits.length; s++){
+            for(int r = 0; r <ranks.length; r++){
+                myCards[index] = new Card(ranks[r], suits[s]);
+                index++;
+            }
+        }
     }
     
     private void shuffle(){
@@ -31,7 +37,9 @@ public class Deck {
     
     //printDeck is for testing only --delete me
     public void printDeck(){
-        
+        for(int i = 0; i < myCards.length; i++){
+            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT);
+        }
     }
     
     public Card dealCard(){ //a class is the data type of an object
